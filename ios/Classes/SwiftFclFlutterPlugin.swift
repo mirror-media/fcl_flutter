5c3d3de83f79f3df41de0cf0991bedc6cefd2c32
Sam Feng <sam0880517@gmail.com>
2022-10-12 11:56:07 +0800

    feat(iOS): implement iOS function

--- ios/Classes/SwiftFclFlutterPlugin.swift
+++ ios/Classes/SwiftFclFlutterPlugin.swift
@@ -1,5 +1,9 @@
 import Flutter
 import UIKit
+import FCL_SDK
+import Foundation
+import Cadence
+import FlowSDK
 
 public class SwiftFclFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,6 +13,139 @@ public class SwiftFclFlutterPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+      switch(call.method){
+        case "initFCL":
+          let arguments = call.arguments as! [String: Any]
+          let bloctoSDKAppId = arguments["bloctoAppId"] as! String
+          let useTestNet = arguments["useTestNet"] as! Bool
+          initFCL(bloctoSDKAppId: bloctoSDKAppId, useTestNet: useTestNet)
+          result("Initial success")
+        case "simpleLogin":
+          Task{
+              let address = await simpleLogin()
+              if(address == "error"){
+                  result(FlutterError.init(code: "SimpleLoginFailed", message: "Get user account address failed", details: nil))
+              }else{
+                  result(address)
+              }
+          }
+        case "accountProofLogin":
+          let arguments = call.arguments as! [String: Any]
+          let appIdentifier = arguments["appIdentifier"] as! String
+          Task{
+              let address = await accountProofLogin(appIdentifier: appIdentifier)
+              if(address == "error"){
+                  result(FlutterError.init(code: "AccountProofLoginFailed", message: "Get user account address failed", details: nil))
+              }else{
+                  result(address)
+              }
+          }
+        case "verifyAccountProof":
+          let arguments = call.arguments as! [String: Any]
+          let appIdentifier = arguments["appIdentifier"] as! String
+          
+          if(fcl.currentUser?.accountProof != nil){
+              let accountProof = fcl.currentUser!.accountProof!
+              Task {
+                  let isValid = await verifyAccountProof(appIdentifier: appIdentifier, accountProof: accountProof)
+                  if(isValid != nil){
+                      result(isValid)
+                  }else{
+                      result(FlutterError.init(code: "VerifyAccountProofFailed", message: "Verify accountProof failed", details: nil))
+                  }
+              }
+          }else{
+              print("AccountProofData is nil")
+              result(FlutterError.init(code: "NoAccountProofData", message: "AccountProofData is nil", details: nil))
+          }
+
+          
+        default:
+          result(FlutterMethodNotImplemented)
+    }
+      
   }
+    
+    private func initFCL(bloctoSDKAppId: String, useTestNet: Bool){
+        do {
+            let bloctoWalletProvider = try BloctoWalletProvider(
+                bloctoAppIdentifier: bloctoSDKAppId,
+                window: nil,
+                testnet: useTestNet
+            )
+            fcl.config
+                .put(.network(useTestNet ? .testnet : .mainnet))
+                .put(.supportedWalletProviders(
+                    [
+                        bloctoWalletProvider,
+                    ]
+                ))
+            print("Initial FCL success")
+        } catch {
+            print("Initial FCL error")
+            print(error)
+        }
+    }
+    
+    private func simpleLogin() async -> String{
+        var accountAddress: String
+        do{
+            accountAddress = try await fcl.login().hexStringWithPrefix
+        }catch{
+            print("Simple login error")
+            print(error)
+            accountAddress = "error"
+        }
+      return accountAddress
+    }
+    
+    private func accountProofLogin(appIdentifier: String) async -> String{
+        var accountAddress: String
+        let nonce = getNonce()
+        if(nonce != nil){
+            let accountProofData = FCLAccountProofData(
+                appId: appIdentifier,
+                nonce: nonce! // minimum 32-byte random nonce as a hex string.
+            )
+            do{
+                accountAddress = try await fcl.authanticate(accountProofData: accountProofData).hexStringWithPrefix
+            }catch{
+                print("AccountProof login error")
+                print(error)
+                accountAddress = "error"
+            }
+        }else{
+            print("AccountProof login error")
+            accountAddress = "error"
+        }
+        return accountAddress
+    }
+    
+    private func getNonce() -> String?{
+        var keyData = Data(count: 32)
+        let result = keyData.withUnsafeMutableBytes {
+            SecRandomCopyBytes(kSecRandomDefault, 32, $0.baseAddress!)
+        }
+        if result == errSecSuccess {
+            return keyData.toHexString()
+        } else {
+            print("Problem generating random bytes")
+            return nil
+        }
+    }
+    
+    private func verifyAccountProof(appIdentifier: String,accountProof: AccountProofSignatureData) async -> Bool?{
+        do {
+            let valid = try await AppUtilities.verifyAccountProof(
+                appIdentifier: appIdentifier,
+                accountProofData: accountProof,
+                fclCryptoContract: nil
+            )
+            return valid
+        } catch {
+            print("Verify accountProof error")
+            print(error)
+            return nil
+        }
+    }
 }
--- ios/fcl_flutter.podspec
+++ ios/fcl_flutter.podspec
@@ -5,19 +5,20 @@
 Pod::Spec.new do |s|
   s.name             = 'fcl_flutter'
   s.version          = '0.0.1'
-  s.summary          = 'A new Flutter plugin project.'
+  s.summary          = 'A blocto Flutter plugin project.'
   s.description      = <<-DESC
-A new Flutter plugin project.
+A new Flutter plugin project for blocto sdk.
                        DESC
   s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'Mirror Media' => 'sam0517@mirrormedia.mg' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '9.0'
+  s.dependency 'FCL-SDK', '0.1.6'
+  s.platform = :ios, '13.0'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
-  s.swift_version = '5.0'
+  s.swift_version = '5.6'
 end
