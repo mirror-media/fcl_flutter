5d63407dac3594ca184437f5cf764a0686efd4c6
Sam Feng <sam0880517@gmail.com>
2022-11-08 16:17:04 +0800

    feat(example): add query, mutate, and READrCoin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
@@ -5,7 +6,9 @@ import 'package:fcl_flutter/fcl_flutter.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -21,6 +24,8 @@ class _MyAppState extends State<MyApp> {
   final _fclFlutterPlugin = FclFlutter();
   String? _accountBalance = 'Unknown';
   String _alert = "";
+  String? _queryResult;
+  String? _transactionId;
 
   @override
   void initState() {
@@ -37,83 +42,107 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('FCL_Flutter'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (_alert.isNotEmpty)
-                Text(
-                  _alert,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.red),
-                ),
-              if (_accountAddress == null)
-                const Text(
-                  'Please login',
-                  textAlign: TextAlign.center,
-                )
-              else ...[
-                Text(
-                  'Account address: $_accountAddress',
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'AccountProof verify: $_isVerified',
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'Balance: $_accountBalance',
-                  textAlign: TextAlign.center,
-                ),
-              ],
-              const SizedBox(
-                height: 50,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FCL_Flutter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_alert.isNotEmpty)
+              Text(
+                _alert,
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.red),
               ),
-              Wrap(
-                alignment: WrapAlignment.center,
-                spacing: 10,
-                children: [
-                  if (_accountAddress == null) ...[
-                    ElevatedButton(
-                      onPressed: () async => await simpleLogin(),
-                      child: const Text('Simple login'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async => await accountProofLogin(),
-                      child: const Text('AccountProof login'),
-                    ),
-                  ] else ...[
-                    ElevatedButton(
-                      onPressed: () async => await verifyAccountProof(),
-                      child: const Text('Verify accountProof'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        await _fclFlutterPlugin.unauthenticate();
-                        setState(() {
-                          _accountAddress = null;
-                        });
-                      },
-                      child: const Text('Unauthenticate'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async => await getAccountBalance(),
-                      child: const Text('Get account balance'),
-                    ),
-                  ],
-                  ElevatedButton(
-                    onPressed: () async => await getAddress(),
-                    child: const Text('Get account address'),
-                  ),
-                ],
+            if (_queryResult != null)
+              Text(
+                'Query result: $_queryResult',
+                textAlign: TextAlign.center,
+              ),
+            if (_transactionId != null)
+              Text(
+                'Transaction ID: $_transactionId',
+                textAlign: TextAlign.center,
+              ),
+            if (_accountAddress == null)
+              const Text(
+                'Please login',
+                textAlign: TextAlign.center,
+              )
+            else ...[
+              Text(
+                'Account address: $_accountAddress',
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'AccountProof verify: $_isVerified',
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'Balance: $_accountBalance',
+                textAlign: TextAlign.center,
               ),
             ],
-          ),
+            const SizedBox(
+              height: 50,
+            ),
+            Wrap(
+              alignment: WrapAlignment.center,
+              spacing: 10,
+              children: [
+                if (_accountAddress == null) ...[
+                  ElevatedButton(
+                    onPressed: () async => await simpleLogin(),
+                    child: const Text('Simple login'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async => await accountProofLogin(),
+                    child: const Text('AccountProof login'),
+                  ),
+                ] else ...[
+                  ElevatedButton(
+                    onPressed: () async => await verifyAccountProof(),
+                    child: const Text('Verify accountProof'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await _fclFlutterPlugin.unauthenticate();
+                      setState(() {
+                        _accountAddress = null;
+                      });
+                    },
+                    child: const Text('Unauthenticate'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async => await getAccountBalance(),
+                    child: const Text('Get account balance'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async => await testMutate(context),
+                    child: const Text('Use mutate send 1.01 Flow'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async => await createREADrVault(context),
+                    child: const Text('Create READr vault'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async => await getREADrBalance(),
+                    child: const Text('Get READrCoin balance'),
+                  ),
+                ],
+                ElevatedButton(
+                  onPressed: () async => await getAddress(),
+                  child: const Text('Get account address'),
+                ),
+                ElevatedButton(
+                  onPressed: () async => await testQuery(),
+                  child: const Text('Test query 7+6'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -200,6 +229,213 @@ class _MyAppState extends State<MyApp> {
 
     setState(() {
       _accountBalance = balance;
+    });
+  }
+
+  Future<void> testQuery() async {
+    const String script = '''
+    pub fun main(a: Int, b: Int): Int {
+      return a + b
+    }
+    ''';
+    final List<String> arguments = [
+      '''
+      {
+        "type": "Int",
+        "value": "7"
+      }
+      ''',
+      '''
+      {
+        "type": "Int",
+        "value": "6"
+      }
+      ''',
+    ];
+    String? result;
+    try {
+      result =
+          await _fclFlutterPlugin.query(script: script, arguments: arguments);
+      _alert = '';
+    } on PlatformException {
+      _alert = 'Query failed';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _queryResult = result;
+    });
+  }
+
+  Future<void> testMutate(BuildContext context) async {
+    String? address = await showDialog<String>(
+      context: context,
+      builder: (context) {
+        String? input;
+        return AlertDialog(
+          title: const Text('Input address you want to send'),
+          content: TextField(
+            onChanged: (value) => input = value,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, null),
+              child: const Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Colors.redAccent,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, input),
+              child: const Text('Submit'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (address != null) {
+      String script = '''
+      import FungibleToken from 0x9a0766d93b6608b7
+
+      transaction(amount: UFix64, to: Address) {
+        let vault: @FungibleToken.Vault
+        
+        prepare(currentUser: AuthAccount) {
+          self.vault <- currentUser
+            .borrow<&{FungibleToken.Provider}>(from: /storage/flowTokenVault)!
+            .withdraw(amount: amount)
+        }
+
+        execute {
+          getAccount(to)
+            .getCapability(/public/flowTokenReceiver)!
+            .borrow<&{FungibleToken.Receiver}>()!
+            .deposit(from: <- self.vault)
+        }
+      }
+      ''';
+
+      final List<String> arguments = [
+        '''
+        {
+          "type": "UFix64",
+          "value": "1.01"
+        }
+        ''',
+        '''
+        {
+          "type": "Address",
+          "value": "$address"
+        }
+        ''',
+      ];
+
+      String? result;
+      try {
+        result = await _fclFlutterPlugin.mutate(
+            script: script, arguments: arguments);
+        _alert = '';
+        if (kDebugMode) {
+          print('Transaction ID: $result');
+        }
+      } on PlatformException {
+        _alert = 'Mutate failed';
+      }
+
+      if (!mounted) return;
+
+      setState(() {
+        _transactionId = result;
+      });
+    }
+  }
+
+  Future<void> createREADrVault(BuildContext context) async {
+    String script = '''
+      import FungibleToken from 0x9a0766d93b6608b7
+      import ReadrCoin from 0x191deed43d01c59c
+      transaction {
+        prepare(signer: AuthAccount) {
+          // Return early if the account already stores a ReadrCoin Vault
+          if signer.borrow<&ReadrCoin.Vault>(from: ReadrCoin.VaultStoragePath) != nil {
+            return
+          }
+          // Create a new ReadrCoin Vault and put it in storage
+          signer.save(
+            <-ReadrCoin.createEmptyVault(),
+            to: ReadrCoin.VaultStoragePath
+          )
+          // Create a public capability to the Vault that only exposes
+          // the deposit function through the Receiver interface
+          signer.link<&ReadrCoin.Vault{FungibleToken.Receiver}>(
+            ReadrCoin.ReceiverPublicPath,
+            target: ReadrCoin.VaultStoragePath
+          )
+          // Create a public capability to the Vault that only exposes
+          // the balance field through the Balance interface
+          signer.link<&ReadrCoin.Vault{FungibleToken.Balance}>(
+            ReadrCoin.BalancePublicPath,
+            target: ReadrCoin.VaultStoragePath
+          )
+        }
+      }
+      ''';
+
+    String? result;
+    try {
+      result = await _fclFlutterPlugin.mutate(script: script);
+      _alert = '';
+      if (kDebugMode) {
+        print('Transaction ID: $result');
+      }
+    } on PlatformException {
+      _alert = 'Mutate failed';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _transactionId = result;
+    });
+  }
+
+  Future<void> getREADrBalance() async {
+    const String script = '''
+    import FungibleToken from 0x9a0766d93b6608b7
+    import ReadrCoin from 0x191deed43d01c59c
+    pub fun main(account: Address): UFix64 {
+      let acct = getAccount(account)
+      let vaultRef = acct.getCapability(ReadrCoin.BalancePublicPath)
+        .borrow<&ReadrCoin.Vault{FungibleToken.Balance}>()
+        ?? panic("Could not borrow Balance reference to the Vault")
+      return vaultRef.balance
+    }
+    ''';
+    final List<String> arguments = [
+      '''
+      {
+        "type": "Address",
+        "value": "$_accountAddress"
+      }
+      ''',
+    ];
+    String? result;
+    try {
+      result =
+          await _fclFlutterPlugin.query(script: script, arguments: arguments);
+      _alert = '';
+    } on PlatformException {
+      _alert = 'Get READr balance failed';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _accountBalance = '$result READrCoin';
     });
   }
 }
